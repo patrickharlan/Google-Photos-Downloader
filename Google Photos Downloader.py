a9686cc6576d5a7a1250a085828e0a0b1a826384
Tristan Huen <huentristan@gmail.com>
2023-10-15 02:11:45 -0700

    Changed photo download to async. Now faster.

--- Google Photos Downloader.py
+++ Google Photos Downloader.py
@@ -3,10 +3,12 @@ import pickle
 import time
 import subprocess
 import os
-import requests
 import piexif
 import pytz
 import concurrent.futures
+import asyncio
+import aiohttp
+from tqdm.asyncio import tqdm_asyncio
 from PIL import Image
 from typing import Iterator
 from datetime import datetime
@@ -17,9 +19,25 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 #TO DO:
-#-Possibly use multiprocessing/multithreading for photo downloading
+#-Fix issues with tqdm and async/multithreading. Bar seems to just jump.
+#-Clean up code (e.g. rename variables to better reflect content).
+
+#NOTES:
+#-It may be a good idea to use an async google api library like aiogoogle. Currently the service object
+# takes a significant amount of time to get a response.
+#
+#-The multithreaded code for grabbing album media could be possibly rewritten using async instead. Though
+# this may not be the effort.
+#
+#-Currently the list of photos not apart of an album and list of photos possessing no EXIF data are global
+# variables. It would be better to just pass them in as arguments to the functions but I don't see any
+# benefit in doing that as of the moment.
 
 load_dotenv()
+list_no_exif = [] 
+list_no_album = []
+FILE_DIR = os.getenv('PHOTO_DIRECTORY')
+TIME_NAME_FORMAT = "%Y-%m-%d %H.%M.%S"
 
 def utc_to_pt(utc_string:'str',format:'str'="%Y:%m:%d %H:%M:%S",return_obj:'bool'=False) -> str | datetime:
     """
@@ -33,7 +51,7 @@ def utc_to_pt(utc_string:'str',format:'str'="%Y:%m:%d %H:%M:%S",return_obj:'bool
     utc_string (string): UTC date whose format is %Y-%m-%dT%H:%M:%SZ.
     format (string), optional: The string date format of the returned PT date.
     return_obj (boolean), optional: If true then it will return the PT date as a datetime object. Else it will
-                                   return it as a string.
+                                    return it as a string.
   
     Returns
     ----------
@@ -95,7 +113,7 @@ def get_media(service, params:dict, start_date:datetime = datetime(2022,8,24)) -
 
 def get_album_thread(creds,album:dict, start_date:datetime = datetime(2022,8,24))-> list[dict]:
     """
-    Thread function for getting album
+    Thread function for getting album content
   
     Function to be used in ThreadPoolExecutor to get album content.
   
@@ -143,6 +161,122 @@ def get_album_thread(creds,album:dict, start_date:datetime = datetime(2022,8,24)
     media_content = [content for content in get_media(service,{'albumId':album['id'], 'pageSize':20},start_date=start_date)]
     media_content.insert(0,album_id) #Multithreading does not guarantee order so we use the album id for reordering later.
     return media_content
+
+
+async def save_media(photo:dict, session:aiohttp.ClientSession) -> None:
+    """
+    Function for saving a single photo/video from API to folder.
+  
+    Async function that downloads the photo/video from the API, modifies its name to
+    give the date and time taken, and saves it to a folder based on the one it matched in
+    Google Photos. Any media that is not organized automatically gets put in a "Not Organized"
+    folder. Additionally, photos that do not have EXIF data will automatically have data 
+    generated.
+  
+    Parameters
+    ----------
+    photo (dict) : Dictionary returned by the Google Photos API.
+    session (aiohttp.ClientSession) : Aiohttp session object for async HTTP requests.
+    """
+
+    no_exif = False
+    filename = photo['filename']
+    file_extension = filename[filename.rfind('.'):].lower()
+    folder_name = photo['album']
+    creation_time = photo['mediaMetadata']['creationTime']
+    is_video = (folder_name == "Videos" or file_extension == '.mov' or file_extension == '.mp4')
+    actual_date = utc_to_pt(creation_time)
+
+    #The '=dv' and '=d' download the videos and photos respectively in full quality and properly
+    if (is_video):
+        media_url = photo['baseUrl'] + '=dv'
+    else:
+        media_url = photo['baseUrl'] + '=d'
+ 
+    async with session.get(media_url) as response:
+
+        photo_bytes = await response.content.read()
+
+    if (folder_name == "Not in any album"):
+        list_no_album.append(datetime.strptime(actual_date,"%Y:%m:%d %H:%M:%S").strftime(TIME_NAME_FORMAT) + file_extension.upper())
+        folder_name = 'Not Organized'
+
+    #Just for downloading it to the correct folder. Name will be changed at end of function.
+    file_original_path = FILE_DIR + folder_name + '\\' + filename
+
+    with open(file_original_path, 'wb') as f:
+        f.write(photo_bytes)
+
+        if (not is_video):
+            im = Image.open(file_original_path)
+
+            try:
+                exif_dict = im.info['exif']
+            except KeyError:  # Some photos may not have EXIF data
+                list_no_exif.append((datetime.strptime(actual_date,"%Y:%m:%d %H:%M:%S").strftime(TIME_NAME_FORMAT) + file_extension.upper(), actual_date))
+                no_exif = True
+            else:
+                # Apply correct date from metadata
+                exif_dict = piexif.load(exif_dict)
+                piexif.remove(file_original_path)
+                encoded_date = actual_date.encode('utf-8')
+                exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal] = encoded_date
+                exif_dict['Exif'][piexif.ExifIFD.DateTimeDigitized] = encoded_date
+
+                # Apply changes to photos
+                exif_bytes = piexif.dump(exif_dict)
+                piexif.insert(exif_bytes, file_original_path)
+    
+    if (no_exif):
+        exif_dict = {
+            "0th": {},
+            "Exif": {
+                piexif.ExifIFD.DateTimeOriginal: actual_date,
+                piexif.ExifIFD.DateTimeDigitized: actual_date
+            },
+            "GPS": {},
+            "Interop": {},
+            "1st": {},
+            "thumbnail": None
+        }
+        exif_bytes = piexif.dump(exif_dict)
+        im.save(FILE_DIR + folder_name + '\\' +filename, exif=exif_bytes)
+        im.close()
+
+        #Only way to properly change the PNG time which is used by Windows instead of the new EXIF for some reason.
+        if(filename.find("PNG")):
+            subprocess.run(["exiftool", "-overwrite_original", f"-PNG:CreationTime={actual_date}", file_original_path],
+                            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    if(not is_video):
+        im.close()
+
+    actual_date = datetime.strptime(actual_date,"%Y:%m:%d %H:%M:%S").strftime(TIME_NAME_FORMAT)
+
+    # The following uses replace instead of rename since replace accounts for already existing files
+    os.replace(file_original_path, FILE_DIR +  folder_name + '\\' + actual_date + file_extension)
+
+async def save_medias(photos:list[dict]) -> None:
+    """
+    Function for saving photos/videos from API to their corresponding folder.
+  
+    Async function that downloads photos/videos from the API. Uses a "pool" of tasks to 
+    concurrently download the media without them blocking eachother
+  
+    Parameters
+    ----------
+    photo (list[dict]) : List of dictionaries which are returned by the Google Photos API.
+    """
+
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+
+        for photo in photos:
+            task = asyncio.ensure_future(save_media(photo,session))
+            tasks.append(task)
+
+        await tqdm_asyncio.gather(*tasks, desc="Downloading Photos")
+
 
 #AUTH Code Found Here: https://stackoverflow.com/questions/58928685/google-photos-api-python-working-non-deprecated-example
 
@@ -193,7 +327,7 @@ search_params = {
 
 service._resourceDesc['resources']['mediaItems']['methods']['search']['parameters'].update(search_params)
 
-start_date = datetime(2023,2,6)
+start_date = datetime(2023,8,25)
 
 photos = [content for content in get_media(service,{'pageSize':20},start_date)]
 
@@ -210,20 +344,21 @@ album_id_list = [album['id'] for album in albums]
 
 album_media = []
 
-#Don't think with_alive progress supports multithreading. Another alternative such as tqdm may be possible.
 start = time.time()
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    futures = [executor.submit(get_album_thread, creds,album,start_date) for album in albums]
-    concurrent.futures.wait(futures)
+#Grab all album media, satisfying the start date, for each album using multithreading.
+with tqdm(total=len(albums), desc = "-> Grabbing album media...") as pbar:
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(get_album_thread, creds,album,start_date) for album in albums]
 
-    # Wait for all tasks to finish
-    for future in concurrent.futures.as_completed(futures):
-        result = future.result()
-        album_media.append(result)
+        # Wait for all tasks to finish
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            album_media.append(result)
+            pbar.update(1)
+end = time.time()
+print(f"Album media download time: {end - start}s")
 
-print(time.time() - start)
-
-#The album_media is out of order so we can use the album_id_list to reorder it based 
+#The album_media is out of order so we can use the album_id_list to reorder it based on the id 
 album_media = sorted(album_media, key=lambda element: album_id_list.index(element[0]))
 # print(album_media)
 
@@ -274,87 +409,15 @@ for photo in photos:
     else:
         photo['album'] = titles
 
-# print(photos)
+print(photos[0])
 
-list_no_exif = []
-list_no_album = []
-file_dir = os.getenv('PHOTO_DIRECTORY')
+start = time.time()
+loop = asyncio.get_event_loop()
+future = asyncio.ensure_future(save_medias(photos))
+results = loop.run_until_complete(future)
+end = time.time()
 
-#Download all photos/videos and change the dates/titles for them to match up
-for photo in tqdm(photos, desc = "-> Downloading please wait..."):
-    request = service.mediaItems().get(mediaItemId=photo['id'])
-    response = request.execute()
-
-    #The '=dv' and '=d' download the videos and photos respectively in full quality and properly
-    if (photo['album'] == "Videos"):
-        media_url = response['baseUrl'] + '=dv'
-    else:
-        media_url = response['baseUrl'] + '=d'
-
-    response = requests.get(media_url)
-    photo_bytes = response.content
-
-    is_video = False
-    no_exif = False
-    no_album = False
-    filename = photo['filename']
-    folder_name = photo['album']
-    creation_time = photo['mediaMetadata']['creationTime']
-    actual_date = utc_to_pt(creation_time)
-
-    if (folder_name == "Not in any album"):
-        list_no_album.append(filename)
-        folder_name = 'Not Organized'
-
-    with open(file_dir + folder_name + '\\' + filename, 'wb') as f:
-        f.write(photo_bytes)
-
-        if (folder_name == "Videos"):
-            is_video = True
-        else:
-            im = Image.open(file_dir + folder_name + '\\' + filename)
-
-            try:
-                exif_dict = im.info['exif']
-            except KeyError:  # Some photos may not have EXIF data
-                list_no_exif.append((filename, actual_date))
-                no_exif = True
-            else:
-                # Apply correct date from metadata
-                exif_dict = piexif.load(exif_dict)
-                piexif.remove(file_dir + folder_name + '\\' + filename)
-                encoded_date = actual_date.encode('utf-8')
-                exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal] = encoded_date
-                exif_dict['Exif'][piexif.ExifIFD.DateTimeDigitized] = encoded_date
-
-                # Apply changes to photos
-                exif_bytes = piexif.dump(exif_dict)
-                piexif.insert(exif_bytes, file_dir + folder_name + '\\' + filename)
-
-    if (no_exif):
-        exif_dict = {
-            "0th": {},
-            "Exif": {
-                piexif.ExifIFD.DateTimeOriginal: actual_date,
-                piexif.ExifIFD.DateTimeDigitized: actual_date
-            },
-            "GPS": {},
-            "Interop": {},
-            "1st": {},
-            "thumbnail": None
-        }
-        exif_bytes = piexif.dump(exif_dict)
-        im.save(file_dir + folder_name + '\\' +filename, exif=exif_bytes)
-        im.close()
-        #Only way to properly change the PNG time which is used by Windows instead of the new EXIF for some reason.
-        if(filename.find("PNG")):
-            subprocess.run(["exiftool", "-overwrite_original", f"-PNG:CreationTime={actual_date}", file_dir + folder_name + '\\' + filename],
-                            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    
-    # The following uses replace instead of rename since replace accounts for already existing files
-    im.close()
-    actual_date = datetime.strptime(actual_date,"%Y:%m:%d %H:%M:%S").strftime("%Y-%m-%d %H.%M.%S")
-    os.replace(file_dir + folder_name + '\\' + filename, file_dir +  folder_name + '\\' + actual_date + filename[filename.rfind('.'):])
+print(f"Photo Download Time {end-start}s")
 
 if(list_no_exif):
     print(f"The following image{'s' if (len(list_no_exif) > 1) else ''} had no EXIF data and data was created automatically:")
@@ -365,7 +428,3 @@ if(list_no_album):
     for i in range(len(list_no_album)):
         print(list_no_album[i])
 print("NOTE: PNG images may use the tag of CreationTime instead of the supplied EXIF. This is accounted for.")
-
-
-
-
